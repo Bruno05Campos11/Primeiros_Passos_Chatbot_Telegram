46c23148c970103b7970a43344d1683b37d1c967
Bruno Campos <bruno05campos11@gmail.com>
2024-08-26 08:37:43 -0300

    menu no start

---
+++ queremos_promos_teste_bot.py
@@ -0,0 +1,64 @@
+import telebot
+from modulo_chave_api import RetornarChave
+
+chave_api = RetornarChave()
+bot = telebot.TeleBot (chave_api)
+
+#----------------------------------------------------------------------------
+menu = """
+Clique em uma das opções abaixo:
+/imprimir - Imprime as informações de sua mensagem no cmd
+/nome - Veja que sei o seu nome
+"""
+#----------------------------------------------------------------------------
+@bot.message_handler (commands=["start"])
+
+def ResponderSalve (mensagem):
+	enviar = "Comigo você pode pegar ofertas personalizadas!\n" + menu
+	bot.send_message (mensagem.chat.id, enviar)
+
+#----------------------------------------------------------------------------
+@bot.message_handler (commands=["salve"])
+
+def ResponderSalve (mensagem):
+	bot.reply_to (mensagem, "salve mano")
+
+#----------------------------------------------------------------------------
+def VerificarMensagem(mensagem):
+	if mensagem.text == "como vc tá?":
+		return True
+	else:
+		return False
+
+@bot.message_handler (func=VerificarMensagem)
+
+def ResponderToBem (mensagem):
+	bot.reply_to (mensagem, "tô bem, e vc?")
+
+#----------------------------------------------------------------------------
+@bot.message_handler (commands=["imprimir"])
+
+def ImprimirInformacoes (mensagem):
+	print (mensagem)
+	bot.send_message (mensagem.chat.id, "imprimi as informações da sua mensagem")
+
+#----------------------------------------------------------------------------
+@bot.message_handler(commands=["nome"])
+
+def DescobrirNome (mensagem):
+	enviar = "seu nome é " + mensagem.chat.first_name
+	bot.send_message (mensagem.chat.id, enviar)
+
+#----------------------------------------------------------------------------
+def QualquerMensagem(mensagem):
+		return True
+
+@bot.message_handler (func=QualquerMensagem)
+
+def ResponderMenu (mensagem):
+	enviar = "Não tenho resposta para isso, confira meus comandos no menu\n" +menu
+	bot.reply_to (mensagem, enviar)
+
+#----------------------------------------------------------------------------
+
+bot.polling()
